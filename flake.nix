7032a71e973bac00409d6934c8c6fa029760fcbe
Mátyás Makay <mmakay94@gmail.com>
2025-03-02 20:34:53 +0100

    Initial NixOS configuration

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "NixOS system configuration with flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+      };
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          # Import your existing configuration
+          ./configuration.nix
+
+          # Integrate home-manager
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.mmakay = import ./home.nix;
+          }
+        ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should manage.
+  home.username = "mmakay";
+  home.homeDirectory = "/home/mmakay";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  home.stateVersion = "24.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Packages to install
+  home.packages = with pkgs; [
+    vscode
+    spotify
+    obsidian
+    telegram-desktop
+  ];
+
+  # Git configuration
+  programs.git = {
+    enable = true;
+    userName = "Mátyás Makay";
+    userEmail = "mmakay94@gmail.com";
+    
+    # Additional Git configurations
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull.rebase = false;
+      color.ui = true;
+      
+      # You can add more git configurations here
+      # For example:
+      # core.editor = "vim";
+      # merge.tool = "vimdiff";
+    };
+    
+    # Optional: Configure Git aliases
+    aliases = {
+      co = "checkout";
+      ci = "commit";
+      st = "status";
+      br = "branch";
+      hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
+    };
+    
+    # Optional: Configure Git ignores globally
+    ignores = [ 
+      ".DS_Store" 
+      "*.swp" 
+      ".env" 
+      "node_modules"
+      ".vscode/"
+      ".idea/"
+    ];
+  };
+}
