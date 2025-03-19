cf652f47bb2447d0b1a2cbad3d9afd2729503260
Mátyás Makay <mmakay94@gmail.com>
2025-03-19 16:45:21 +0100

    remove python314 from the list of installed packages

--- configuration.nix
+++ configuration.nix
@@ -178,7 +178,6 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     discord
-    python314
     git
     home-manager
   ];
